05239bb6a2f7325d2c67fa1f4825bfa9f6722f85
JuliezerGithub <129005402+JuliezerGithub@users.noreply.github.com>
2023-04-11 12:43:41 -0300

    Programa3 Funcional
    Adicionado do VSCode

--- Lista_2/src/Lista_2/Programa3.java
+++ Lista_2/src/Lista_2/Programa3.java
@@ -1,25 +1,24 @@
-package Lista_2;
-
 import java.util.Scanner;
-/*
- * 3) Crie uma classe “Programa3” que receba do usuário um valor par, e apresente os próximos 10 números pares. 
- */
-public class Programa3 {
-	public static void main (String [] arg) {//Recebe o número digitado pelo usuáio
-		float num = 0.0f;
-		try (Scanner sc = new Scanner(System.in)) {
-		System.out.println("Digite um número para saber os próximos 10 números pares: ");
-		num = sc.nextFloat();
-		}
-		if (num%2 == 0) {
-			System.out.println("\nO numero "+num+" é par!");
 
-			while (num <=  10) {
-				System.out.println("\n "+num);
-				num = num + 2;
-			}
-			} else {
-			System.out.println("\nO numero "+num+" é impar. Reinicie o programa e insira um valor par!");
-			}
-	}
+public class Programa3 {
+
+    public static void main(String[] arg) {// Recebe o número digitado pelo usuáio
+        float num = 0.0f;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Digite um número para saber os próximos 10 números pares: ");
+            num = sc.nextFloat();
+        }
+        if (num % 2 == 0) {
+            System.out.println("\nO numero " + num + " é par!");
+            int i = 0;
+            while (i <= 10) {
+                num = num + 2;
+                System.out.println("\n " + num);
+
+                i++;
+            }
+        } else {
+            System.out.println("\nO numero " + num + " é impar. Reinicie o programa e insira um valor par!");
+        }
+    }
 }
