a8c7fafe614de3733c0b295c5ed0ed874a87151a
JuliezerGithub <129005402+JuliezerGithub@users.noreply.github.com>
2023-04-12 22:48:47 +0000

    Inclusão da Classe Variável

--- Programação Orientada a Objetos/Lista_2/src/Lista_2/Programa6.java
+++ Programação Orientada a Objetos/Lista_2/src/Lista_2/Programa6.java
@@ -1,6 +1,6 @@
 package Lista_2;
 
-/*6) Escreva uma Programa chamado “Programa6” que calcule a soma dos números de 1 a 
+/*6 Escreva uma Programa chamado “Programa6” que calcule a soma dos números de 1 a 
 15. Utilize o comando de repetição Enquanto (While), depois utilize o comando Faça 
 Enquanto (Do.. While) e por fim, o comando Para (For). 
 */
@@ -8,33 +8,23 @@ Enquanto (Do.. While) e por fim, o comando Para (For).
 public class Programa6 {
 	
 	public static void main(String [] args) 
-	{
-			
-		//Declarando as variáveis
-		int Inicial = 1, Final = 15;
-		int contador1 = 1;
-		int contador2 = 1;
-		
-		System.out.println("Calculo usando wilhe: ");
-		while (contador1 <= 14) {
-		Final = Final + Inicial;
-		Inicial++;
-		contador1++;
-		}
-		System.out.println("A soma de 1 até 15 é: "+Final);
-		
-		Inicial = 1;
-		Final = 15;
-		System.out.println("\nCalculo usando Do While: ");
-		
-		do {
-			Final = Final + Inicial;
-			Inicial++;
-			contador2++;	
-		}
-		while(contador2 < 15);
-		System.out.println("A soma de 1 até 15 é: "+Final);	
-		
+	{	
+		Variaveis valores1 = new Variaveis();
+			valores1.vinicial = 1;
+			valores1.vfinal = 15;
+			valores1.contador = 1;
+			valores1.metodo = "While";
+		Variaveis valores2 = new Variaveis();
+			valores2.vinicial = 1;
+			valores2.vfinal = 15;
+			valores2.contador = 1;
+			valores2.metodo = "Do While";
+		Variaveis valores3 = new Variaveis();
+			valores3.vinicial = 1;
+			valores3.vfinal = 15;
+			valores3.contador = 1;
+
+		valores1.medotoWhile();
+		valores2.metodoDoWhile();
 	}
-	
 }
---
+++ Programação Orientada a Objetos/Lista_2/src/Lista_2/Variaveis.java
@@ -0,0 +1,31 @@
+package Lista_2;
+
+public class Variaveis {
+		//Declarando as variáveis
+		int vinicial;
+        int vfinal;
+        int contador;
+        String metodo;
+
+void medotoWhile() {
+    System.out.println("Calculo usando "+metodo+": ");
+		while (contador <= 14) {
+		vfinal = vfinal + vinicial;
+		vinicial++;
+		contador++;
+		}
+    System.out.println("A soma de 1 até 15 é: "+vfinal+"\n");
+    }
+void metodoDoWhile() {
+    do {
+        
+        vfinal = vfinal + vinicial;
+        vinicial++;
+        contador++;	
+    }
+    while(contador < 15);
+    System.out.println("Calculo usando "+metodo+": ");
+    System.out.println("A soma de 1 até 15 é: "+vfinal);	
+    
+}
+}
