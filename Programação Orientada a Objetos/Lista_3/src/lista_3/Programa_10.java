e5499b2c943fc2f0ffab5edb2be059ae15bf026a
JuliezerGithub <129005402+JuliezerGithub@users.noreply.github.com>
2023-05-30 12:24:59 -0300

    Adicionado Programa 10

--- Programação Orientada a Objetos/Lista_3/src/lista_3/Programa_10.java
+++ Programação Orientada a Objetos/Lista_3/src/lista_3/Programa_10.java
@@ -1,5 +1,26 @@
 package lista_3;
+/*10. Crie um programa que receba um nome, e coloque cada letra do nome dentro de uma posição de
+um vetor. Faça o vetor imprimir em ordem inversa.*/
+import java.util.Scanner;
 
 public class Programa_10 {
+	public static void main(String[] args) {
 
+		try (Scanner scanner = new Scanner(System.in)) {
+				// Preenche o vetor com o nome informado
+				System.out.println("Digite um nome para inverter a ondem das letras: ");
+				String nome = scanner.nextLine();
+				
+				char[] vetor = new char[nome.length()];
+				
+				for (int i = 0; i < nome.length(); i++) {
+					vetor[i] = nome.charAt(i);
+				}
+		System.out.println("Invertendo o nome: ");
+		for(int i = vetor.length - 1; i >=0; i-- ) {
+		System.out.print(vetor[i]);
+		}
+		}
+	}
 }
+
